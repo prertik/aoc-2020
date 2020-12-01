264f378a6ba24aaf5b7370d85048b2e172d8d625
Pratik Karki <predatoramigo@gmail.com>
2020-12-01 14:19:20 +0545

    Day 1

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,28 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.java.io :refer [resource]]
+            [clojure.string :as string]))
+
+(def data (string/split-lines (slurp (resource "day-01.txt"))))
+
+(def rd-data (map read-string data))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (->> (for [num1 input
+           num2 input
+           :when (= (apply + [num1 num2]) 2020)]
+        [num1 num2])
+      first
+      (reduce *)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (->> (for [num1 input
+             num2 input
+             num3 input
+             :when (= (apply + [num1 num2 num3]) 2020)]
+         [num1 num2 num3])
+       first
+       (reduce *)))
--- test/advent_of_code/day_01_test.clj
+++ test/advent_of_code/day_01_test.clj
@@ -1,12 +1,17 @@
 (ns advent-of-code.day-01-test
   (:require [clojure.test :refer [deftest testing is]]
             [advent-of-code.day-01 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :as string]))
+
+(defn obtain-format
+  [file]
+  (map read-string (string/split-lines (slurp (resource file)))))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "day-01-example.txt")))))))
+  (let [expected 1018944]
+    (is (= expected (part-1 (obtain-format "day-01.txt"))))))
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "day-01-example.txt")))))))
+  (let [expected 8446464]
+    (is (= expected (part-2 (obtain-format "day-01.txt"))))))
