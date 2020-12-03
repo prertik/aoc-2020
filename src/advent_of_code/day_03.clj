a982e1cf8d66f838977da38b1998efa0da94403d
Pratik Karki <predatoramigo@gmail.com>
2020-12-03 19:09:21 +0545

    Day 03

--- src/advent_of_code/day_03.clj
+++ src/advent_of_code/day_03.clj
@@ -1,11 +1,34 @@
-(ns advent-of-code.day-03)
+(ns advent-of-code.day-03
+  (:require [clojure.java.io :refer [resource]]
+            [clojure.string :as string]))
+
+(def data (slurp (resource "day-03.txt")))
+
+(defn count-trees
+  ([line dx dy]
+   (let [width (count (first line))
+         height (count line)
+         get-range (map (fn [y x] [y (mod (* x dx) width)])
+                        (range 0 height dy)
+                        (range))]
+     (->> get-range
+          (map (fn [[y x]] (nth (nth line y) x)))
+          (filter #(= % \#))
+          (count))))
+  ([line] (count-trees line 3 1)))
+
 
 (defn part-1
   "Day 03 Part 1"
   [input]
-  input)
+  (->> input
+       (string/split-lines)
+       (count-trees)))
 
 (defn part-2
   "Day 03 Part 2"
   [input]
-  input)
+  (let [graph (string/split-lines input)]
+    (reduce * (map (fn [dx dy] (count-trees graph dx dy))
+                   [1 3 5 7 1]
+                   [1 1 1 1 2]))))
--- test/advent_of_code/day_03_test.clj
+++ test/advent_of_code/day_03_test.clj
@@ -4,9 +4,9 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "day-03-example.txt")))))))
+  (let [expected 294]
+    (is (= expected (part-1 (slurp (resource "day-03.txt")))))))
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "day-03-example.txt")))))))
+  (let [expected 5774564250]
+    (is (= expected (part-2 (slurp (resource "day-03.txt")))))))
